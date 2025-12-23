a792fd420dcf38c22750b920ad08505d091c991d
oofeyang22 <olaitanferanmi41@gmail.com>
2025-12-23 11:49:59 -0800

    updated code

--- my-app/src/components/hero/Hero.tsx
+++ my-app/src/components/hero/Hero.tsx
@@ -6,9 +6,9 @@ import { motion } from 'framer-motion';
 const Hero = () => {
     return (
         <motion.div
-        initial={{ opacity: 0, y: -100 }}
+        initial={{ opacity: 0, y: -80 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
+        transition={{ duration: 0.1 }}
         viewport={{ once: true }}
         >
             <div className={styles.heroContainer}>
